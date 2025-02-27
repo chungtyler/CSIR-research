ba50e1adcf9bf925ec46e5a41d22b7c0e90721df
Administrator <administrator@cpr-yor44.cpr-yor44>
2025-02-27 18:19:39 -0500

    ollama remote

--- scripts/models/deepseek.py
+++ scripts/models/deepseek.py
@@ -1,3 +1,30 @@
+import os
+os.environ['OLLAMA_HOST'] = "130.63.251.161:11434"
+from ollama import chat
+
 class DeepSeek:
     def __init__(self):
-        pass
+        self.model_name = 'deepseek-r1:7b'
+
+    def get_response(self, prompt, query):
+        response = chat(
+            model=self.model_name, 
+            messages=[
+                {"role": "system", "content": prompt},
+                {'role': 'user', 'content': query}
+            ]
+        )
+
+        response = response['message']['content']
+        return response
+
+
+if __name__ == "__main__":
+    system_prompt = "Assume you are a doctor skilled in treating patients."
+    query = "I feel stomachache, can you help me?"
+
+    ollma_client = DeepSeek()
+    res = ollma_client.get_response(system_prompt, query)
+    print(res)
+
+
